bff4bceeb653c96513c9fa4f36675c7755f56376
amberlynn <amberlynn364@gmail.com>
2024-03-04 16:14:36 +0300

    feat: implement depth-limit tests

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema, gqlSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
 import { createPrismaContext } from './context.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -16,13 +17,15 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     async handler(req) {
       const { prisma } = fastify;
       const { query, variables } = req.body;
-      const response = await graphql({
+      const errorData = validate(gqlSchema, parse(query), [depthLimit(5)]);
+      if (errorData.length) return { errors: errorData };
+
+      return graphql({
         schema: gqlSchema,
         source: query,
         variableValues: variables,
         contextValue: createPrismaContext(prisma)
       });
-      return response;
     },
   });
 };
